993c54f9164c015bd57f370e5b3f3b76214d36da
Tyuiu.VdovichenkoAI@outlook.com <a.vdovichenko10@gmail.com>
2023-11-05 23:19:45 +0500

    Task3

---
+++ Tyuiu.VdovichenkoAI.Sprint4.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VdovichenkoAI.Sprint4.Task3.V9.Lib
+{
+    public class DataService : ISprint4Task3V9
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1;
+            int columns = array.Length / rows;
+            int sum = 0;
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                {
+                    sum += array[row, 1];
+                }
+            }
+            return sum;
+        }
+    }
+}
