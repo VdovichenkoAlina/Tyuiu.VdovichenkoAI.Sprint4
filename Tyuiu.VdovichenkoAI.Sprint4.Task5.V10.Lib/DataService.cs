32e7cb594d54d753c4e550625a82f3e598f7d756
Tyuiu.VdovichenkoAI@outlook.com <a.vdovichenko10@gmail.com>
2023-11-12 23:41:37 +0500

    Task5

---
+++ Tyuiu.VdovichenkoAI.Sprint4.Task5.V10.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.VdovichenkoAI.Sprint4.Task5.V10.Lib
+{
+    public class DataService : ISprint4Task5V10
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int collumn = matrix.Length / rows;
+            int count = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        count += matrix[i, j];
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.VdovichenkoAI.Sprint4.Task5.V10.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VdovichenkoAI.Sprint4.Task5.V10.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VdovichenkoAI.Sprint4.Task5.V10.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8ab52ac-1420-4db7-8855-def8912fdef1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
